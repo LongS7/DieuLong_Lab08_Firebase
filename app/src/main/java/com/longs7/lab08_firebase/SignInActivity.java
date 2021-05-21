0c97fe2362d34d02614fbd738b83dfc0999abaaf
longs7 <longkeyvic1@gmail.com>
2021-05-21 14:22:59 +0700

    add SignInActivity

--- app/src/main/java/com/longs7/lab08_firebase/SignInActivity.java
+++ app/src/main/java/com/longs7/lab08_firebase/SignInActivity.java
@@ -2,13 +2,47 @@ package com.longs7.lab08_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private EditText edtEmail;
+    private EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        
+        edtEmail = findViewById(R.id.etEmail);
+        edtPassword = findViewById(R.id.etPassword);
+        
+        findViewById(R.id.btnSignIn).findViewById(R.id.btnSignIn).setOnClickListener(v -> {
+            String email = edtEmail.getText().toString().trim();
+            String password = edtPassword.getText().toString().trim();
+
+            if(email.isEmpty() || password.isEmpty())
+                Toast.makeText(this, "Email and password are not empty!", Toast.LENGTH_SHORT).show();
+            else {
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(authResult -> {
+                            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        })
+                        .addOnFailureListener(e -> Toast.makeText(SignInActivity.this, "Invalid email or password!", Toast.LENGTH_SHORT).show());
+            }
+        });
+
+        findViewById(R.id.tvRegister).setOnClickListener(v -> {
+            Intent intent = new Intent(SignInActivity.this, RegisterActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
