6d0c3defa981f71d5974e687670fed4f033643ea
longs7 <longkeyvic1@gmail.com>
2021-05-21 13:58:24 +0700

    add ui

---
+++ app/src/main/java/com/longs7/lab08_firebase/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.longs7.lab08_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
---
+++ app/src/main/java/com/longs7/lab08_firebase/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.longs7.lab08_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
