f6c7cc0d79c1a47fb9e5a450e0e25f38320875d3
longs7 <longkeyvic1@gmail.com>
2021-05-21 14:10:24 +0700

    add StartActivity

--- app/src/main/java/com/longs7/lab08_firebase/StartActivity.java
+++ app/src/main/java/com/longs7/lab08_firebase/StartActivity.java
@@ -1,8 +1,13 @@
 package com.longs7.lab08_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +15,28 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        FirebaseAuth.getInstance().addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //If user has logged in
+                //Go to main activity
+                if(firebaseAuth.getCurrentUser() != null){
+                    Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+
+        findViewById(R.id.btnSignIn).setOnClickListener(v -> {
+            Intent intent = new Intent(StartActivity.this, SignInActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.btnRegister).setOnClickListener(v -> {
+            Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
     }
 }
